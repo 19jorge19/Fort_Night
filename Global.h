08b2760fdad16a0c91de18e3d48ae4a3f0bbb113
jnord02 <152651372+jnord02@users.noreply.github.com>
2024-02-28 10:33:40 -0600

    Global.h
    global header file, to be imported into every header file. keeps track of health thirst and hunger throughout code

---
+++ Global.h
@@ -0,0 +1,18 @@
+//global header file, to be imported into every header file, named Global.h
+//includes global variables of health thirst and hunger
+
+#pragma once
+
+#ifndef global
+#define global
+
+extern int health;
+extern int thirst;
+extern int hunger;
+
+#endif
+
+
+
+
+
