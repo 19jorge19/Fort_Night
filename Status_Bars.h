04bf4c03849491d8015fb254c574472957ad6a29
jnord02 <152651372+jnord02@users.noreply.github.com>
2024-02-28 10:31:55 -0600

    Status_Bars.h
    header file for status bars

---
+++ Status_Bars.h
@@ -0,0 +1,21 @@
+//Status bars header file, named Status_Bars.h
+
+
+#pragma once
+#include <stdio.h>
+#include "Global.h"
+
+// include header file connection to main/other functions
+
+void modifyhealth(int factor);
+void modifythirst(int factor);
+void modifyhunger(int factor);
+
+
+
+
+
+
+
+
+
