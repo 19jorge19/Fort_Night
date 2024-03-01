934db5596e19a72d9492c208731ae50623d76266
jnord02 <152651372+jnord02@users.noreply.github.com>
2024-03-01 11:31:21 -0600

    Update Global.h

--- Global.h
+++ Global.h
@@ -1,11 +1,12 @@
-//  Global header file, to be imported into every header file, named Global.h
-//  includes global variables of health thirst and hunger
+//global header file, to be imported into every header file, named Global.h
+//includes global variables of health thirst and hunger
 
 #pragma once
 #pragma warning(disable: 4996)
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #ifndef global
 #define global
@@ -14,8 +15,8 @@ extern int health;
 extern int thirst;
 extern int hunger;
 extern int days;
-//extern double food_chance;
-//extern double water_chance;
+extern double food_chance;
+extern double water_chance;
 
 #endif
 
