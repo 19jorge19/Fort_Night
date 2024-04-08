130eca8133ae346faf5c68aafb23b861b716141c
BogStandardOldMan <maxagamble@gmail.com>
2024-04-08 11:15:32 -0500

    Update Main_Menu.c

--- Main_Menu.c
+++ Main_Menu.c
@@ -15,7 +15,35 @@ void main_menu()
     int choice;
     scanf("%d", &choice);
 
-    switch (choice)
+
+    if (choice == 0) {
+        printf("Thanks for playing\n");
+        exit(0);
+    }
+    else if (choice == 1) {
+        return;
+    }
+    else if (choice == 2) {
+        //write out instructions
+        printf("Welcome to Fortnight the decision-making survival game.\n");
+        printf("To start playing, please select the play option.\n");
+        printf("You will be given options every day to choose from.\n"); //change to three a day if we change that
+        printf("Each one of these options will impact your likelyhood of\n");
+        printf("survival, so be careful. Additionally there will be special events you \n");
+        printf("must react to in order to survive!\nGood Luck!\n\n");
+        main_menu();
+    }
+    else if (choice == 3) {
+        //write out credits
+        printf("Developers:\n");
+        printf("Dylan\nJorge\nJosh\nMax\nXavier\n\n");
+        main_menu();
+    }
+    else {
+        printf("Please choose a valid choice.\n");
+        main_menu();
+    }
+   /* switch (choice)
     {
     case 0:
         printf("Thanks for playing\n");
@@ -40,6 +68,7 @@ void main_menu()
         printf("Please choose a valid choice.\n");
         main_menu();
     }
+    */
 }
 
 
